06474cd7be5bea332036f136993ce62ee4fb7012
KlipsScripts <klipbitz@gmail.com>
2026-06-27 02:47:13 -0400

    loader!

---
+++ loader.lua
@@ -0,0 +1,24 @@
+function antiAntiCheat()
+	if (game.PlaceId == 93911318070665) then -- apoc 2
+		game.Players.LocalPlayer.PlayerGui:WaitForChild("Rules"):Destroy()
+	end
+	print("anti anti cheat finished running")
+end
+
+function loadModule(id)
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/KlipsScripts/BaconHub/refs/heads/main/".. id .. ".lua", true))()
+end
+
+function loadAllModules() 
+	local games = {"mm2", "jailbreak", "prisonlife", "crewmates"}
+	
+	loadModule("main")
+	wait(1)
+	for _, game in pairs(games) do
+		loadModule(game)
+	end
+end
+
+antiAntiCheat()
+wait(2)
+loadAllModules()
